c621d390f745bfaf216223afbec970a684be036b
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-20 07:46:02 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alpha;
+
+        for (alpha = 'z'; alpha >= 'a'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+}

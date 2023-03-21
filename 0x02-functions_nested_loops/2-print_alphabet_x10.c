465470d53f02087322b2f41486a2123aeed883d8
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-21 10:16:16 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,5 +15,5 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			putchar(alpha);
-	putchar('\n');
+		putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _islower -  checks for lowercase character
+ *
+ *@c: Variable
+ *Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

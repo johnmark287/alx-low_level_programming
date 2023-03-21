47f2ce7fd51d2245012605815d4f4bf7bb1113bf
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-21 06:12:22 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_alphabet_x10-Entry point
+ *
+ * Description: prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			putchar(alpha);
+	putchar('\n');
+}

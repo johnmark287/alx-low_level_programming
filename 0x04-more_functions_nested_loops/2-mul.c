df61b955bbbcb56d52ad7e3cd78064605b509e1e
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-26 08:46:59 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * mul - multiplies two integers
+ *
+ * @a: Character to be multiplied.
+ *
+ * @b: Character to be multiplied.
+ *
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

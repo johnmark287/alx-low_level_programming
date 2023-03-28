79ec2681252c560bcf81cd86da10030de76c3e99
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-28 03:17:26 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets n to 98.
+ *
+ * @n: the value to be reset.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#Pointers, arrays and strings
+How to use and manipulate pointers, arrays and strings.

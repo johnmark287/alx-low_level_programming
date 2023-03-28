1dff5b5ceee3da2dd8cf547bf02527875c094d91
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-28 04:38:17 -0700

    his report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -  returns the length of a string.
+ *
+ * @s: string to print its length.
+ *
+ * Return: length of x.
+ */
+int _strlen(char *s)
+{
+	char x = strlen(s);
+	return x;
+}

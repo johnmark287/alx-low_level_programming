55d390c449a7a37d9112717495344f25232ff925
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-28 19:38:44 -0700

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 
 /**
  * _strcpy - copies the string pointed to by src,
- *  including the terminating null byte (\0), 
+ *  including the terminating null byte (\0),
  *  to the buffer pointed to by dest.
  *
  *  @dest: first parameter.
@@ -14,9 +14,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	if (dest == NULL)
-		return NULL;
+		return (NULL);
+
 
 	i = 0;
 	while (*(src + i))

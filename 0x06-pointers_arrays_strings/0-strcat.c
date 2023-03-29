6a8658e6d92f31c0b33e9e3d96aab965e24c297d
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-29 08:59:45 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,18 @@
 #include "main.h"
+#include<string.h>
 /**
  * _strcat - concatenates two strings.
+ *
  * @dest: string 1.
  *
- * @scr: string 2.
+ * @src: string 2.
  *
  * Return: void.
  */
+char *_strcat(char *dest, char *src)
+{
+	char *x;
 
+	x = strcat(dest, src);
+	return (x);
+}

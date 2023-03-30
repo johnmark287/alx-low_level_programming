39059a6793e87a1823b6bdbf7b3594038bbfeddd
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-29 21:46:31 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @dest: string 1.
+ *
+ * @src: string 2.
+ *
+ * @n: number of bytes from src.
+ *
+ * return: char.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strncpy - copies a string.
+ *
+ *

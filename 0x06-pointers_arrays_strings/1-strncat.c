14efd847b96b0dea1261babc27edec1774f61d01
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-30 10:01:55 -0700

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,4 +13,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int dest_len, i;
 
+	dest_len = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}

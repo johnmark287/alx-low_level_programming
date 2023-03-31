89935bbb5b792960bb16338c877523e2e07ae288
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-30 23:45:16 -0700

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,33 +2,21 @@
 #include<string.h>
 
 /**
- * _strcmp - copys n bytes of string 1 to tring 2.
+ * _strcmp - compares string 1 and string 2.
  *
  * @s1: string 1.
  *
  * @s2: string 2.
  *
- * Return: -15, 15, or 0.
+ * Return: int.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, n, len1, len2;
+	int i;
 
 	i = 0;
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (len1 >= len2)
-		n = len2;
-	else
-		n = len1;
-	while (s1[i] == s2[i] && i < n)
-	{
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 +i)))
 		i++;
-	}
-	if (s1[i] == s2[i])
-		return (0);
-	else if (s1[i] < s2[i])
-		return (-15);
-	else
-		return (15);
+
+	return (*(s1 + i) - *(s2 + i));
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,23 +10,25 @@
 char *string_toupper(char *s)
 {
         int i;
-	char a;
+        char a, b;
 
-	i = 0;
-	a = 'a';
+        i = 0;
+        a = 'a';
+        b = 'A';
         while (*(s + i) != '\0')
         {
                 if (s[i] >= 'a' && s[i] <= 'z')
                 {
                         while (*(s + i) != '\0')
-			{
-				while (s[i] == a)
-				{
-					s[i] = a;
-					a++;
-				}
-			}
-		}
-	}
-	return (s);
+                        {
+                                while (s[i] == a)
+                                {
+                                        s[i] = b;
+                                        a++;
+                                        b++;
+                                }
+                        }
+                }
+        }
+        return (s);
 }

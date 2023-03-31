2e4ba2dbe703278e2eeeec5a7747327c830cd7db
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-30 22:51:27 -0700

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<string.h>
+
 /**
  * _strcmp - copys n bytes of string 1 to tring 2.
  *
@@ -17,9 +18,9 @@ int _strcmp(char *s1, char *s2)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	if (len1 >= len2)
-		n = len1;
-	else
 		n = len2;
+	else
+		n = len1;
 	while (s1[i] == s2[i] && i < n)
 	{
 		i++;

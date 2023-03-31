ee851b9c3553524b8de99877515b733e0c38a0e9
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-03-31 00:26:55 -0700

    commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,14 @@
  */
 char *string_toupper(char *s)
 {
-        int i;
+	int i;
 
-        i = 0;
-        while (*(s + i))
-        {
-                if (*(s + i) >= 'a' && *(s + i) <= 'z')
-                {
-			*(s + i) -= ('a' - 'A');
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= 'a' - 'A';
 			i++;
 		}
 	}

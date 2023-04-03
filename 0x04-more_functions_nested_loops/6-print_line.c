d7b289071d3cc5401cc0243fb04b8881735a44f1
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-04-03 09:21:12 -0700

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,17 @@
 #include "main.h"
 /**
- * print_line - draws a straight line in the terminal
+ * print_line - prints an underscore n times
+ * @n: number of times printed
  *
- * Return: Always (0).
+ * Return: void
  */
 void print_line(int n)
 {
-	int i;
-
-	if (n > 0)
-	{
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
 }
+_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,22 @@
 #include "main.h"
 /**
- * print_diagonal - draws a diagonal line on the terminal.
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal
  *
- * @n: number of times the character \ should be printed.
- *
- * Return: No return.
+ * Return: void
  */
 void print_diagonal(int n)
 {
-	char i, j;
-
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+int a, b;
+if (n <= 0)
+_putchar('\n');
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
 }

95894cbc29c2bb4f6a25d867651b2a4c2ff79cb0
MUHANDO johnmark <johnmark.muhando@students.jkuat.ac.ke>
2023-04-11 13:19:46 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,17 +10,21 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int i;
+	unsigned int len, i;
 	char *ptr;
 
 	if (str == NULL)
 		return (NULL);
 
-	ptr = malloc(sizeof(char) * (strlen(str) + 1));
+	len = 0;
+	while(str[len])
+		len++;
+	len++;
+	ptr = malloc(sizeof(char) * len);
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < (strlen(str)); i++)
+	for (i = 0; i < len; i++)
 		ptr[i] = str[i];
 
 	return (ptr);

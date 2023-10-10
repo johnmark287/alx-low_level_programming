6f2c0f3ed34bed3ef1370d879a71c759d4be7852
johnmark297 <johnmarkmuhando@students.jkuat.ac.ke>
2023-10-10 20:56:59 +0300

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,33 +1,10 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - entry point
+ * Return: 0 Always success
  */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-/**
- * main - prints the name of the file it was compiled from,
- * followed by a new line.
- * Return: 0 on success, integer error on failure.
-*/
 int main(void)
 {
-int i;
-char *str;
-
-str = __FILE__;
-i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }

6b3ef34dfc452bd8c57a4344cc50d7d4e42014fe
johnmark297 <johnmarkmuhando@students.jkuat.ac.ke>
2023-10-10 11:01:33 +0300

    Structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees all memory allocated by new_dog().
+ * @d: pointer to the new_dog struct.
+ * Return: void.
+*/
+void free_dog(dog_t *d)
+{
+if (d == NULL || d->name == NULL || d->owner == NULL)
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}

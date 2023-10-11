c473596ffd2253c4045271dea1c252d1a933db6a
johnmark297 <johnmarkmuhando@students.jkuat.ac.ke>
2023-10-12 00:53:50 +0300

    Advance Pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,11 @@
 #include "function_pointers.h"
 /**
- * 
+ * int_index -  searches for an integer.
+ * @array: Array to search elelment.
+ * @size: Array size.
+ * @cmp: Pointer to perform action function.
+ * Return: index to the match element on success,
+ * -1 on failure.
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {

2110bdca49595f74401790ed995f588e287af349
johnmark297 <johnmarkmuhando@students.jkuat.ac.ke>
2023-10-13 19:57:28 +0300

    commandline calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,23 @@
 #include "3-calc.h"
-int main(int argc, int **argv)
+#include <stdlib.h>
+int main(int argc, char **argv)
 {
-	int i, j;
+	int a, b, c;
+	int (*f)(int, int);
 	if (argc != 4)
-		printf("Error");
-
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	c = f(a ,b);
+	printf("%d\n", c);
 	return (0);
 }

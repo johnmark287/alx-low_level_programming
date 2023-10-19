b6b3f44980e14b9bffbb49c795143fc9c67841ab
Q.u.a.n <117285113+johnmark287@users.noreply.github.com>
2023-10-19 23:21:32 +0300

    linked list

---
+++ test/linked_list.c
@@ -0,0 +1,23 @@
+#include <sndio.h>
+#include <stdlib.h>
+struct node 
+{
+	int value;
+	Node *next;
+};
+
+typedef struct node Node;
+
+int main(void)
+{
+	Node *node_ptr = NULL;
+	Node list;
+
+	node_ptr = malloc(sizeof(Node));
+	node_ptr->value = 0;
+	node_ptr->next = NULL;
+
+	list.
+
+	return (0);
+}

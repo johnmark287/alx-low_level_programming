7268ded6ca0fe76a943102b449d1fa82746874a6
Q.u.a.n <117285113+johnmark287@users.noreply.github.com>
2023-10-20 11:05:11 +0300

    Advanced tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first (void) __attribute__ ((constructor));
+
+/**
+* first - prints something before main.
+*
+* Return: void.
+*/
+void first(void)
+{
+	printf("You're beat! and yet, youmust allow,\n");
+	printf("I bore y house upon my back!\n");
+}

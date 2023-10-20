1183052fff9c34428b0a029b7ee1800bff1cd327
Q.u.a.n <117285113+johnmark287@users.noreply.github.com>
2023-10-20 09:59:26 +0300

    Updates

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
 #include "list.h"
+/**
+* list_len - finds the number of nodes
+* in a linked list.
+* @h: pointer to the start of linked list.
+* Return: Number of elemens in the linked list
+*/
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	unsigned int i;
+
+	temp = h;
+	for (i = 0; temp; i++)
+		temp = temp->next;
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* free_list - free single linked list.
+* @head: pointer to the first node of
+* linked list.
+* Return: nothing.
+*/
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}

786e30dedf3854457051012c831e6e81322164cb
Q.u.a.n <117285113+johnmark287@users.noreply.github.com>
2023-10-20 09:07:30 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,16 @@
 #include "list.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t i;
+    list_t *ptr;
+
+    if (list_t == NULL)
+        printf("[0] (nil)");
+
+    ptr = h;
+    for (i = 0; ptr != NULL; i++)
+        ptr = ptr->h;
+
+    return (i);
+}

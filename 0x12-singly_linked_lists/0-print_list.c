0910acc21bd91db1fa48f7f27e8d02958fe85ff2
Q.u.a.n <117285113+johnmark287@users.noreply.github.com>
2023-10-20 09:24:09 +0300

    Number of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,24 @@
 #include "list.h"
-
+/**
+* print_list - print size of linked list elements.
+* @h: pointer to first node of list.
+* Return: Number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
-    size_t i;
-    list_t *ptr;
+	size_t count = 1;
 
-    if (list_t == NULL)
-        printf("[0] (nil)");
-
-    ptr = h;
-    for (i = 0; ptr != NULL; i++)
-        ptr = ptr->h;
-
-    return (i);
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nill)");
+		else
+			printf("[%d] %s\n", 0, h->len, h->str);
+		h = h->next;
+		count += 1;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return (count);
 }

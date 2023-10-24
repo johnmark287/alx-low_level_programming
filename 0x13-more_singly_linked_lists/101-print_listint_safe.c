b9969ef6f407207103fc0bbc5eb2aaafa2a39bb7
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-24 15:30:23 +0300

    alx

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list.
+ * @list: the old list to append.
+ * @size: size of the new list(always one more than the new list).
+ * @new: new node to add to the list.
+ * Return: pointer to the new list.
+*/
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append.
+ * @size: size of the new list(always one more than the old list)
+ * @new: node to add to the new list.
+ * Return: pointer to the new list.
+*/
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}

0fdf60650c0feace534d4894d1f34331a2df54d4
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-30 22:36:18 +0300

    file create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,5 +14,25 @@ size_t _strlen(char *s)
 }
 
 /**
- * create_file
+ * create_file - creates a file.
+ * @filename: name of file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 in failure.
 */
+int create_file(const char *filename, char *text_content);
+{
+	int fd;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
+		return (-1);
+	return (1);
+}

c428eb927d5355a8528632a8338e4e495c7676db
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-30 22:44:13 +0300

    file append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,5 +2,38 @@
 
 /**
  * _strlen - find string length.
- * @str
+ * @s: pointer to the string.
+ * Return: length of string.
 */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * int append_text_to_file - appends a text at the end of the file.
+ * @filename: name of the file.
+ * @text_conntent: NULL terminated string to add to the file.
+ * Return: 1 on success and -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd;
+    ssize_t len;
+
+    if (filename == NULL)
+        return (-1);
+    fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return (-1);
+    if (text_content)
+        len = write(fd, text_content, _strlen(text_content));
+    close(fd);
+    if (len == -1)
+        return (-1);
+    return (1);
+}

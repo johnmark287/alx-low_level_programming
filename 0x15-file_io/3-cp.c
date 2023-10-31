02162e24567740816b3c6b8e3f5a6492adbf1643
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-31 21:01:02 +0300

    update elf header

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,15 +22,17 @@ void check97(int argc)
  * @fd_to:: file descriptor of file_to, or -1.
  * Return: void.
 */
-void check98(int check, char *file, int fd_from, int fd_to)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
-	dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file);
-	if (fd_from != 1)
-		close(fd_from);
-	if (fd_to != -1)
-		close(fd_to);
-	exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file);
+		if (fd_from != 1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
 }
 /**
  * check99 - checks that file_to was created and/or
@@ -44,12 +46,14 @@ void check98(int check, char *file, int fd_from, int fd_to)
 void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: can't write to file %s\n", file);
-	if (fd_from != -1)
-		close(fd_from);
-	if (fd_to != 1)
-		close(fd_to);
-	exit(99);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != 1)
+			close(fd_to);
+		exit(99);
+	}
 }
 
 /**
@@ -61,8 +65,10 @@ void check99(ssize_t check, char *file, int fd_from, int fd_to)
 void check100(int check, int fd)
 {
 	if (check == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: can't close file descriptor %d\n", fd);
-	exit(100);
+		exit(100);
+	}
 }
 
 /**
@@ -82,10 +88,10 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, file_perm);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
 	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
-	while (lenr > 1024)
+	while (lenr == 1024)
 	{
 		lenr = read(fd_from, buffer, 1024);
 		check98(lenr, argv[1], fd_from, fd_to);

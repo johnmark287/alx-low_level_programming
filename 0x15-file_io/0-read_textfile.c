a200b19df05972aad2465c6806c830c7301c4f89
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-31 15:13:07 +0300

    another update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	if (lenw != lenw)
+	if (lenr != lenw)
 		return (0);
 	return (lenw);
 }
